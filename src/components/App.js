fed7ceb50d142974c90ef647aab6b1c27a377ea8
asonkes <audrey.sonkes@gmail.com>
2026-01-18 00:31:01 +0100

    1ere partie des conditions et des listes DB - lien avec React - fonctionnement

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Banner from './Banner';
+import ShoppingList from './SoppingList';
 import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <ShoppingList/>
       <Cart/>
     </div>
   );
---
+++ src/components/SoppingList.js
@@ -0,0 +1,49 @@
+/********************************/
+/* Notre 3eme composant est créé */
+/********************************/
+
+/** Si on fait :
+ * import '../datas/plantList.js'
+ * ==> on exécute le fichier
+ * ==> On IMPORTE PAS LES DONNEES !!!
+ */
+import { plantList } from '../datas/plantList';
+
+/** Reduce( ) me permet de créer le nouveau tableau qui vient de la DB */
+/** Tableau avec juste 1 fois le nom de chaque catégorie  */ 
+const categories = plantList.reduce((acc, plant) => {
+    /** Si la catégorie est déjà dans l'accumulateur, tu fais rien */
+    if(acc.includes(plant.category)) {
+        /** Si la catégorie est déjà dans l'accumulateur, tu fais rien */
+        return acc
+    } else {
+        /** Sinon on la rajoute */
+        return acc.concat(plant.category);
+    }
+}, []);
+
+/** ici on va ajouter le composant lui-même */
+/** Map() affiche le tableau */
+const ShoppingList = () => {
+    return (
+        <div>
+            <ul>
+                {categories.map(cat => 
+                    <li key={cat}>{cat}</li>
+                )}
+            </ul>
+            <ul>
+                { plantList.map(plant =>
+                    <li key={`${plant.id}`}>{plant.name}</li>
+                )}
+            </ul>
+        </div>
+    );
+}
+
+export default ShoppingList;
+
+/** Avec l'API
+ * Cela donnera :
+ * <li key={plant.id}>{plant.name}</li>
+ */

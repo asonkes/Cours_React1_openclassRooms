ac31f8f7ed2f2d2218560c181c30062e3a4f46f5
asonkes <audrey.sonkes@gmail.com>
2026-01-18 00:58:20 +0100

    petits commentaires en + pour la compréhension

--- src/components/SoppingList.js
+++ src/components/SoppingList.js
@@ -10,6 +10,7 @@
 import { plantList } from '../datas/plantList';
 
 /** Reduce( ) me permet de créer le nouveau tableau qui vient de la DB */
+/** acc = nouveau tableau que l'on va créer au fur et à mesure */
 /** Tableau avec juste 1 fois le nom de chaque catégorie  */ 
 const categories = plantList.reduce((acc, plant) => {
     /** Si la catégorie est déjà dans l'accumulateur, tu fais rien */
@@ -18,6 +19,7 @@ const categories = plantList.reduce((acc, plant) => {
         return acc
     } else {
         /** Sinon on la rajoute */
+        /** concat permet de ne pas modifier le tableau de départ ou ajoute ou pas une donnée au tableau */
         return acc.concat(plant.category);
     }
 }, []);
